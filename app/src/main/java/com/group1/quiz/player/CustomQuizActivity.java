e197c833a402ef8ab106a689bcb44b5afdea4d99
sifaz <sifazhang.nz@gmail.com>
2026-04-29 15:37:18 +1200

    create play's activities

---
+++ app/src/main/java/com/group1/quiz/player/CustomQuizActivity.java
@@ -0,0 +1,29 @@
+package com.group1.quiz.player;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.group1.quiz.R;
+import com.group1.quiz.utils.BaseActivity;
+
+public class CustomQuizActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_custom_quiz);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setHeaderTitle("Customize Quiz");
+    }
+}
